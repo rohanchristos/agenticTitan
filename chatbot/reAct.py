6b8fb64b78fb35959b2ff361237dabeb03533c19
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2026-03-08 02:37:50 +0530

    added from local mch

---
+++ chatbot/reAct.py
@@ -0,0 +1,92 @@
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv  
+from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
+from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
+from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
+from langchain_groq import ChatGroq
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+@tool
+def add(a: int, b:int):
+    """This is an addition function that adds 2 numbers together"""
+
+    return a + b 
+
+@tool
+def subtract(a: int, b: int):
+    """Subtraction function"""
+    return a - b
+
+@tool
+def multiply(a: int, b: int):
+    """Multiplication function"""
+    return a * b
+
+tools = [add, subtract, multiply]
+
+model = ChatGroq(model="llama-3.3-70b-versatile").bind_tools(tools)
+
+
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(content=
+        "You are a helpful AI assistant. When performing math operations, "
+        "always use the provided tools. For multi-step problems, call one tool "
+        "at a time and wait for the result before proceeding to the next step. "
+        "Never compute math yourself — always delegate to tools."
+        "also output the result in ai message"
+    )
+    response = model.invoke([system_prompt] + state["messages"])
+    return {"messages": [response]}
+
+
+def should_continue(state: AgentState): 
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls: 
+        return "end"
+    else:
+        return "continue"
+    
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    },
+)
+
+graph.add_edge("tools", "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 40 + 12 and then multiply the result by 6. also give the meaning of ebit in Fm.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
